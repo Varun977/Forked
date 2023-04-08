457f8e7dff8c6ef2b9c9f8ddf6679bd353aa6292
Varun26j05 <122362317+Varun26j05@users.noreply.github.com>
2023-04-08 14:28:09 +0530

    Update forked.py

--- forked.py
+++ forked.py
@@ -1,6 +1,8 @@
 # this is a file to be forked have some errors
+//first error line
 a = 4
 b = 5 
 //some major changes made
+//here are soem changes that i am king 
 //some test changes (remove these lines)
 print(a+b)
