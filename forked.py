64f81b48ff283cf2d628904ee0ff1540966087c3
Varun26j05 <122362317+Varun26j05@users.noreply.github.com>
2023-04-08 12:26:44 +0530

    Update forked.py

--- forked.py
+++ forked.py
@@ -1,5 +1,5 @@
 # this is a file to be forked have some errors
 a = 4
 b = 5 
-//need to remove this comment --> remove this comment in c++ style
+//need to remove this comment --> remove this comment in c++ style (some changes that i made)
 print(a+b)
